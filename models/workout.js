adfb44793f3039318f46130390b4c43be1b86b75
Edita Pesa <editapesa@gmail.com>
2021-06-08 17:37:11 -0400

    complete index.js. start workout.js

---
+++ models/workout.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+
+
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
