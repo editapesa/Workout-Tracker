86da5d9dbfe2f73e7ea235b3ac5efaa96432f0c2
Edita Pesa <editapesa@gmail.com>
2021-06-08 22:39:17 -0400

    add post route to api.js

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+
+router.post('/api/workouts', (req, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    });
+});
+
+
+
+module.exports = router;
