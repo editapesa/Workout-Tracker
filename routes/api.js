c087f7d84d5a6aae6163c6784cf726c0fab3d77e
Edita Pesa <editapesa@gmail.com>
2021-06-10 16:32:57 -0400

    add delete route for workouts

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,17 @@ router.post('/api/workouts', (req, res) => {
     });
 });
 
+//Look into an aggregate function to add up and return the total duration for each workout.
+//check out: $addFields, $sum operator, aggregate functions
 
+router.delete('/api/workouts', ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+    .then(() => {
+        res.json(true);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    });
+});
 
 module.exports = router;
