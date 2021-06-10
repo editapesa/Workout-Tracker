bdaee9a2d20db0003f084459381c918206911f9c
Edita Pesa <editapesa@gmail.com>
2021-06-10 17:10:33 -0400

    add route to add up and return duration and totals for exercises

--- routes/api.js
+++ routes/api.js
@@ -27,6 +27,18 @@ router.put('/api/workouts/:id', ({ body, params }, res) => {
 //Look into an aggregate function to add up and return the total duration for each workout.
 //check out: $addFields, $sum operator, aggregate functions
 
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
+        { $addFields: { totalDuration: { $sum: '$exercises.duration', }, }, },
+    ])
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    });
+});
+
 router.delete('/api/workouts', ({ body }, res) => {
     Workout.findByIdAndDelete(body.id)
     .then(() => {
