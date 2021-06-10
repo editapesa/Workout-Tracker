edf3306913ffb49c3a1b42c0dece4c09aab2d8fb
Edita Pesa <editapesa@gmail.com>
2021-06-10 17:27:46 -0400

    change date in workout schema to show new date

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    day: { type: Date, default: Date.now },
+    day: { type: Date, default: () => new Date() },
     exercises: [
         {
             type: { type: String, },
