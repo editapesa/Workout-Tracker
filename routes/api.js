141a9c83d5c08a69eebc936d9c019565f496c09c
Edita Pesa <editapesa@gmail.com>
2021-06-10 17:19:39 -0400

    add route to show workouts for last 7 days

--- routes/api.js
+++ routes/api.js
@@ -31,8 +31,23 @@ router.get('/api/workouts', (req, res) => {
     Workout.aggregate([
         { $addFields: { totalDuration: { $sum: '$exercises.duration', }, }, },
     ])
-    .then(dbWorkout => {
-        res.json(dbWorkout);
+    .then((dbWorkouts) => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    });
+});
+
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([
+        { $addFields: { totalDuration: { $sum: '$exercises.duration', }, }, },
+    ])
+    .sort({_id: -1 })
+    .limit(7)
+    .then((dbWorkouts) => {
+        console.log(dbWorkouts);
+        res.json(dbWorkouts);
     })
     .catch(err => {
         res.status(400).json(err)
